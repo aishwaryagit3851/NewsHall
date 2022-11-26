677c7df4492c252282a6467f682988790be82e21
GUJJE AISHWARYA <96921172+aishwaryagit3851@users.noreply.github.com>
2022-11-26 11:34:14 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export class Navbar extends Component {
               <li className="nav-item"><Link className="nav-link" to="/health" >Health</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/science" >Science</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/sports" >Sports</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/technology" >technology</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/technology" >Technology</Link></li>
               </ul>
 
           </div>
